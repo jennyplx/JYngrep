9e8154d92c1c83835c8671392974f99182c4ef81
jennyplx <jenny@primalux.com.sg>
2018-04-20 15:49:23 +0800

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router'
 import { NgModule } from '@angular/core';
-
 
 import { AppComponent } from './app.component';
 import { JokeComponent } from './joke/joke.component';
 import { JokeListComponent } from './joke-list/joke-list.component';
 import { JokeFormComponent } from './joke-form/joke-form.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },  
+  { path: 'jokes', component: JokeListComponent },  
+  { path: 'jokes-list', redirectTo: 'jokes' },
+  { path: '**', component: PageNotFoundComponent },  
+];
 
 @NgModule({
   declarations: [
@@ -13,8 +22,11 @@ import { JokeFormComponent } from './joke-form/joke-form.component';
     JokeListComponent,
     JokeComponent,
     JokeFormComponent,
+    HomeComponent,
+    PageNotFoundComponent,
   ],
   imports: [
+    RouterModule.forRoot(appRoutes,),
     BrowserModule
   ],
   providers: [],
